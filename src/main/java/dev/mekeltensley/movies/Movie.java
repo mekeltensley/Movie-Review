ec04e53d91a4309365c7e73ec57e5f63dbf16d99
Mekel Tensley <mekelintech@gmail.com>
2023-03-09 23:52:37 -0500

    Add model instance variable and appropriate annotation

--- src/main/java/dev/mekeltensley/movies/Movie.java
+++ src/main/java/dev/mekeltensley/movies/Movie.java
@@ -1,15 +1,26 @@
 package dev.mekeltensley.movies;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document (collection = "movies")
 /* This annotation lets the framework know that this class
 *  should present each document in the movies collection */
+@Data // Comes from the lombok project library, useful for generating getter and setters
+@AllArgsConstructor // Annotation for a constructor that takes the private fields as an argument
+@NoArgsConstructor // Annotation that takes no parameters
 public class Movie {
+    @Id
+    // This annotation lets the framework know that the ObjectId instance variable is the
+    // unique identifier of the object within the database
     private ObjectId id;
     private String imdbId;
     private String title;
@@ -18,4 +29,8 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    //This annotation will allow the database to store only the ID of the review and
+    // the reviews will be in a separate collection
+    private List<Review> reviewIds;
 }
